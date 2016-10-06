c0adc4feb584248e42efa0b6924c2cb109db7b51
AnushkaKRajasingha <anushkakrajasingha@gmail.com>
2016-10-06 12:52:25 +0530

    Fix variable decaration issue.
    Fix variable decaration issue. for earlier php versions .

--- tstfullwidth.php
+++ tstfullwidth.php
@@ -24,35 +24,34 @@
  * 1.0.0 - First Release
  *  */
 
-$default_headers = array(
-		'Name' => 'Plugin Name',
-		'PluginURI' => 'Plugin URI',
-		'Version' => 'Version',
-		'Description' => 'Description',
-		'Author' => 'Author',
-		'AuthorURI' => 'Author URI',
-		'TextDomain' => 'Text Domain',
-		'DomainPath' => 'Domain Path',
-		'Network' => 'Network',
-		// Site Wide Only is deprecated in favor of Network.
-		'_sitewide' => 'Site Wide Only',
-		'PluginIcon' => 'Icon URL',
-		'DBVersion' => 'Db Version',
-		'DbRemove' => 'Db Remove',
-		'LICENSE_SERVER_URL' => 'License Srv Url',
-		'LICENSE_SECRET' => 'License Secert',
-		'UserDoc'	=> 'UserDocumentation',
-		'DevDoc' => 'DevDocumentation',
-		'HelpSup' => 'HelpAndSupport',
-		'Environment' => 'Environment'
-);
 
-$_plugindata = get_file_data(__FILE__,$default_headers) ;
-
-
-function tst_fullwidth_scripts() {
+function tst_fullwidth_scripts() {	
+	$default_headers = array(
+			'Name' => 'Plugin Name',
+			'PluginURI' => 'Plugin URI',
+			'Version' => 'Version',
+			'Description' => 'Description',
+			'Author' => 'Author',
+			'AuthorURI' => 'Author URI',
+			'TextDomain' => 'Text Domain',
+			'DomainPath' => 'Domain Path',
+			'Network' => 'Network',
+			// Site Wide Only is deprecated in favor of Network.
+			'_sitewide' => 'Site Wide Only',
+			'PluginIcon' => 'Icon URL',
+			'DBVersion' => 'Db Version',
+			'DbRemove' => 'Db Remove',
+			'LICENSE_SERVER_URL' => 'License Srv Url',
+			'LICENSE_SECRET' => 'License Secert',
+			'UserDoc'	=> 'UserDocumentation',
+			'DevDoc' => 'DevDocumentation',
+			'HelpSup' => 'HelpAndSupport',
+			'Environment' => 'Environment'
+	);
+	
+	$_plugindata = get_file_data(__FILE__,$default_headers) ;
 	$dir = plugin_dir_url( __FILE__ );
-	wp_enqueue_style( 'tsfw-style',$dir.'css/style.css',array(),$_plugindata['Version'],'all');
+	wp_enqueue_style( 'tsfw-style',$dir.'css/style.css',array(), $_plugindata['Version'],'all');
 }
 
 add_action( 'wp_enqueue_scripts', 'tst_fullwidth_scripts' );
